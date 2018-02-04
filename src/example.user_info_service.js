15cde5a3bdf3e4a1f124ef7af720be6f504bae2c
Michael <mvcuccaro@users.noreply.github.com>
2018-02-04 18:51:10 -0500

    Create example.user_info_service.js
    example user info service for providing groups the member is subscribed to.

---
+++ src/example.user_info_service.js
@@ -0,0 +1,15 @@
+(function(){
+  var myapp = angular.module('app', []);
+        
+  myapp.factory('UserInfoService', function(){
+    var service = {
+      getMemberGroups:getMemberGroups
+    }
+
+    function getMemberGroups(){
+      return ['basic', 'admin'];
+    }
+
+    return service;
+  });
+})();
